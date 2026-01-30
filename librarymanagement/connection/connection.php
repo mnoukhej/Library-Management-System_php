ed4daf4bd633467e0d498a9f6ccb34da52b5ee1f
mnoukhej <mnoukhej@gmail.com>
2026-01-30 20:55:18 +0530

    deleted

--- librarymanagement/connection/connection.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "lms");
-
-if (!$conn) {
-    die("Database connection failed: " . mysqli_connect_error());
-}
-?>
--- librarymanagement/model/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(0);
-define("HOSTNAME","localhost");
-define("USERNAME","root");
-define("PASSWORD","");
-define("DATABASE","lms");
-?>
